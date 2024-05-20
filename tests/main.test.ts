0d0b5be7478f19cfa833ff3f9a2c493b1a9533d2
Anton Golomazov <golomazov-tosha2012@yandex.ru>
2024-05-20 16:08:36 +0300

    Remove unused packages from application

--- tests/main.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'vitest';
-import { db } from './mocks/db';
-
-describe('Test group', () => {
-  it.only('Test async fetching', () => {
-    const product = db.product.create({ name: 'MKB' });
-    console.log(db.product.delete({ where: { id: { equals: product.id } } }));
-  });
-});
